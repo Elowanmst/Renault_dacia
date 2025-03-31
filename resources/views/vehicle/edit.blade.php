ad6ad6c54123f2359e6a5d23613847e635ff6a6a
Elowan MESTRES <elowanmestres@gmail.com>
2025-03-31 15:05:05 +0200

    additional cars

--- resources/views/vehicle/edit.blade.php
+++ resources/views/vehicle/edit.blade.php
@@ -27,7 +27,44 @@
             <input type="number" class="form-control" id="price" name="price" value="{{ $vehicle->price }}" required>
         </div>
 
+        <div class="form-group">
+            <label for="picture">{{ __('picture') }}</label>
+            <input type="file" class="form-control" id="picture" name="picture" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="fuel">{{ __('fuel') }}</label>
+            <input type="text" class="form-control" id="fuel" name="fuel" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="description">{{ __('description') }}</label>
+            <input type="textarea" class="form-control" id="description" name="description" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="mileage">{{ __('mileage') }}</label>
+            <input type="number" class="form-control" id="mileage" name="mileage" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="transmission">{{ __('transmission') }}</label>
+            <input type="text" class="form-control" id="transmission" name="transmission" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="puissance">{{ __('puissance') }}</label>
+            <input type="text" class="form-control" id="puissance" name="puissance" value="{{ $vehicle->picture }}" required>
+        </div>
+
+        <div class="form-group">
+            <label for="puissance">{{ __('puissance') }}</label>
+            <input type="text" class="form-control" id="puissance" name="puissance" value="{{ $vehicle->picture }}" required>
+        </div>
+
         <button type="submit" class="btn btn-primary">{{ __('update vehicle') }}</button>
     </form>
 </div>
 @endsection
+
+
