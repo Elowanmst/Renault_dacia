8dab7ab33c60e12baf51912090fdcacc2e7b3a7a
Elowan MESTRES <elowanmestres@gmail.com>
2025-04-10 09:56:54 +0200

    admin via index

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Vehicule;
+use App\Models\Vehicle;
 use App\Models\Horaire;
 use App\Models\Service;
 use Illuminate\Http\Request;
@@ -14,7 +14,8 @@ class HomeController extends Controller
         // Récupérer tous les horaires
         
         $services = Service::all(); 
+        $vehicles = Vehicle::all();
         $horaires = Horaire::all()->keyBy('day'); // Associe chaque horaire à son jour
-        return view('index', compact('horaires', 'services')); // Passe les horaires à la vue
+        return view('index', compact('horaires', 'services', 'vehicles')); // Passe les horaires à la vue
     }  
 }
